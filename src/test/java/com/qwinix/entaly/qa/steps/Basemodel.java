7c1003629b752cec3314e099c90e84832b5ff6c2
koustubhagowda <31979285+koustubhagowda@users.noreply.github.com>
2018-07-16 12:55:07 +0530

    Update Basemodel.java

--- src/test/java/com/qwinix/entaly/qa/steps/Basemodel.java
+++ src/test/java/com/qwinix/entaly/qa/steps/Basemodel.java
@@ -1,31 +1,27 @@
 package com.qwinix.entaly.qa.steps;
+
 public class Basemodel {
 	
 	public int xval;
 	public int yval;
 	public String zval;
-	public int getXval()
-	{
+	public int getXval() {
 		return xval;
 	}
-	public void setXval(int xval)
-	{
+	public void setXval(int xval) {
 		this.xval = xval;
 	}
-	public int getYval()
-	{
+	public int getYval() {
 		return yval;
 	}
-	public void setYval(int yval)
-	{
+	public void setYval(int yval) {
 		this.yval = yval;
 	}
-	public String getZval()
-	{
+	public String getZval() {
 		return zval;
 	}
-	public void setZval(String zval)
-	{
+	public void setZval(String zval) {
 		this.zval = zval;
 	}
+
 }
